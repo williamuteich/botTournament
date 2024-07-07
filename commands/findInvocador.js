b2d15c5bea38c7fb7d92558143178fee0faaed44
williamuteich <williamuteich14@gmail.com>
2024-07-07 03:12:46 -0300

    feat: adicionado comando findinvocador para encontrar jogadores e ajustado comando invacador.

--- commands/findInvocador.js
+++ commands/findInvocador.js
@@ -1,5 +1,4 @@
 const { SlashCommandBuilder } = require('discord.js');
-const dbConnection = require('../database/discordDatabase').client;
 const riotMatchV5 = require('../api/apiRiotAccounts').riotMatchV5;
 const riotMatchData = require('../api/apiRiotAccounts').riotMatchData;
 const fetchRiotAccount = require('../api/apiRiotAccounts').fetchRiotAccount;
@@ -7,33 +6,40 @@ const { showInvocador } = require('../handlers/embeds');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('invocador')
-        .setDescription('Mostra as últimas partidas do jogador.'),
+        .setName('findinvocador')
+        .setDescription('Mostra as últimas partidas do jogador.')
+        .addStringOption(option =>
+            option.setName('gamename')
+                .setDescription('Ex: Farofinha')
+                .setRequired(true))
+        .addStringOption(option =>
+            option.setName('tagline')
+                .setDescription('Ex: BR1')
+                .setRequired(true)),
 
     async execute(interaction) {
         try {
+
             if (!interaction.deferred && !interaction.replied) {
                 await interaction.deferReply({ ephemeral: false });
             }
 
-            const db = dbConnection.db();
-            const invocadorCollection = db.collection('invocadores');
+            const promptName = interaction.options.getString('gamename');
+            const promptTag = interaction.options.getString('tagline');
 
-            const invocadorData = await invocadorCollection.findOne({ userDiscord: interaction.user.id });
-
-
-            if (invocadorData) {
-                const imagemInvocador = interaction.user.avatarURL()
-                const retornoApi = await riotMatchV5(invocadorData.puuid, 5);
+            if(promptName && promptTag) {
+                const buscaPuuid = await fetchRiotAccount(promptName, promptTag);
+                const retornoApi = await riotMatchV5(buscaPuuid.puuid, 5);
                 const returnMatch = await riotMatchData(retornoApi);
 
                 if (Array.isArray(returnMatch)) {
-                    const exampleEmbed = showInvocador(returnMatch, invocadorData, imagemInvocador);
+                    const exampleEmbed = showInvocador(returnMatch, buscaPuuid);
                     await interaction.editReply({ embeds: [exampleEmbed], ephemeral: true });
                 } 
             } 
+
         } catch (error) {
-            console.error('Erro ao buscar o invocador no banco de dados:', error);
+            console.error('Erro ao buscar ao tentar encontrar invocador:', error);
             if (interaction.deferred) {
                 await interaction.editReply('Dados do invocador incorretos ou não existente.');
             }
